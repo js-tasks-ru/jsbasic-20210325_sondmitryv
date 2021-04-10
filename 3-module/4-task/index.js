90de5d57fafb9bcf28263485e93c3f3d5464b0ef
Dson <sondmitryv@gmail.com>
2021-04-10 17:55:27 +0300

    3 module task 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let resultStr = '';
+
+  users.forEach((item, index, array) => {
+    if (item.age <= age) {
+      resultStr += `${item.name}, ${item.balance}\n`;
+    }
+  });
+  return resultStr.slice(0, -1);
 }
