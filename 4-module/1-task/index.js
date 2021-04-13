c5415f607cb28704a11decf0e4df0d12f6a6ff07
Dson <sondmitryv@gmail.com>
2021-04-13 17:19:32 +0300

    4 module task 1 done

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('ul');
+  for (const friendsItem of friends) {
+    list.insertAdjacentHTML('beforeEnd', `<li>${friendsItem.firstName} ${friendsItem.lastName}</li>`);
+  }
+  return list;
 }
