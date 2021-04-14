b2bdf90b4b861f07ac99c99d14ca7aab149d04df
Dson <sondmitryv@gmail.com>
2021-04-14 17:43:12 +0300

    module 5 task 2 done

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+  btn.addEventListener('click', (event) => {
+    if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');
+    } else {
+      text.setAttribute('hidden', '');
+    }
+  });
+
 }
